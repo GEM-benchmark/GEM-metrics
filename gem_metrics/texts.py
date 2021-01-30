c856b8b440137c623a694210af69143a6873643e
Ondrej Dusek <odusek@ufal.mff.cuni.cz>
2021-01-30 23:29:50 +0100

    Adding LC-tokenized-nopunct for references

--- gem_metrics/texts.py
+++ gem_metrics/texts.py
@@ -12,6 +12,8 @@ from pycountry import languages
 class Texts:
     """Holder class for output texts or references."""
 
+    PUNCTUATION = set(string.punctuation)
+
     def __init__(self, key, data):
         self.key = key
         if not isinstance(data, dict):
@@ -41,14 +43,18 @@ class Texts:
         # tokenize & keep a list and a whitespace version
         nltk_ensure_download('tokenizers/punkt')
         tokenize_func = partial(nltk.tokenize.word_tokenize, language=self.language.name.lower())
-        self._tokenized = [([tokenize_func(i) for i in item]
-                            if self.multi_ref
-                            else tokenize_func(item))
-                           for item in self.data]
-        self._ws_tokenized = [([' '.join(i) for i in item]
-                               if self.multi_ref
-                               else ' '.join(item))
-                              for item in self._tokenized]
+        if self.multi_ref:
+            self._tokenized = [[tokenize_func(i) for i in inst] for inst in self.data]
+            self._ws_tokenized = [[' '.join(i) for i in inst] for inst in self._tokenized]
+            self._lc_tokenized = [[[w.lower() for w in ref] for ref in inst] for inst in self._tokenized]
+            self._nopunct_lc_tokenized = [[[w for w in ref if w not in self.PUNCTUATION]
+                                           for ref in inst]
+                                          for inst in self._lc_tokenized]
+        else:
+            self._tokenized = [tokenize_func(ref) for ref in self.data]
+            self._ws_tokenized = [' '.join(ref) for ref in self._tokenized]
+            self._lc_tokenized = [[w.lower() for w in ref] for ref in self._tokenized]
+            self._nopunct_lc_tokenized = [[w for w in ref if w not in self.PUNCTUATION] for ref in self._lc_tokenized]
 
     @property
     def untokenized(self):
@@ -65,6 +71,16 @@ class Texts:
         """Return list of (lists of) lists of tokens."""
         return self._tokenized
 
+    @property
+    def list_tokenized_lower(self):
+        """Return list of (lists of) lists of tokens, lowercased."""
+        return self._lc_tokenized
+
+    @property
+    def list_tokenized_lower_nopunct(self):
+        """Return list of (lists of) lists of tokens, lowercased, excluding punctuation."""
+        return self._nopunct_lc_tokenized
+
     def __len__(self):
         return len(self.data)
 
@@ -72,22 +88,8 @@ class Texts:
 class Predictions(Texts):
     """Data holder class for system outputs/predictions."""
 
-    PUNCTUATION = set(string.punctuation)
-
     def __init__(self, data):
         super().__init__(key='generated', data=data)
-        self._lc_tokenized = [[w.lower() for w in item] for item in self.list_tokenized]
-        self._nopunct_lc_tokenized = [[w for w in item if w not in self.PUNCTUATION] for item in self._lc_tokenized]
-
-    @property
-    def list_tokenized_lower(self):
-        """Return list of lists of tokens, lowercased."""
-        return self._lc_tokenized
-
-    @property
-    def list_tokenized_lower_nopunct(self):
-        """Return list of lists of tokens, lowercased, excluding punctuation."""
-        return self._nopunct_lc_tokenized
 
 
 class References(Texts):
@@ -96,11 +98,13 @@ class References(Texts):
     def __init__(self, data):
         super().__init__(key='target', data=data)
 
+
 class Sources(Texts):
     """Data holder class for sources."""
 
     def __init__(self, data):
         super().__init__(key='source', data=data)
+
 
 class Submission:
     """Data class for multiple submissions."""
