d077575121e7e6580c247c1b8132c0bc8b2a22ef
Aman Madaan <amadaan@cs.cmu.edu>
2021-02-09 11:36:12 -0500

    Factor out test data to inputs.py

---
+++ tests/inputs.py
@@ -0,0 +1,46 @@
+# collection of inputs for testing various metrics
+from gem_metrics.texts import Predictions, References
+
+
+class TestData:
+
+    references = References({"values": [
+        {
+            "target": ["Alimentum is not family-friendly, and is near the Burger King in the city centre."]
+        },
+        {
+            "target": ["There is a place in the city centre, Alimentum, that is not family-friendly."]
+        },
+        {
+            "target": ["There is a house in New Orleans."]
+        }
+    ], "language": "en"})
+
+    empty_references = References({"values": [
+        "",
+        "",
+        ""
+    ], "language": "en"})
+
+    predictions = Predictions({"values": ["Alimentum is a non family-friendly restaurant near Burger King in the city centre.",
+                                          "Alimentum is located in the city centre. It is not family-friendly.",
+                                          "Or Orleans has a home."
+                                          ], "language": "en"})
+
+    identical_predictions = Predictions({"values": [
+        "Alimentum is not family-friendly, and is near the Burger King in the city centre.",
+        "There is a place in the city centre, Alimentum, that is not family-friendly.",
+        "There is a house in New Orleans."
+    ], "language": "en"})
+
+    empty_predictions = Predictions({"values": [
+        "",
+        "",
+        ""
+    ], "language": "en"})
+
+    reversed_predictions = Predictions({"values": [
+        "ertnec ytic eht ni gniK regruB eht raen si dna yldneirf ylimaf ton si mutnemilA",
+        "yldneirf ylimaf ton si taht mutnemilA ertnec ytic eht ni ecalp si erehT",
+        "snaelrO weN ni esuoh si erehT"
+    ], "language": "en"})
--- tests/test_bleu.py
+++ tests/test_bleu.py
@@ -1,64 +1,33 @@
 import unittest
+from tests.inputs import TestData
 from gem_metrics.bleu import BLEU
 from gem_metrics.texts import Predictions, References
 
 
-class NGramTest(unittest.TestCase):
+class BleuTest(unittest.TestCase):
 
     test_rust_tokenizer = True
 
     def setUp(self):
         super().setUp()
-        self.references_identical = [
-            {
-                "target": ["Alimentum is not family-friendly, and is near the Burger King in the city centre."]
-            },
-            {
-                "target": ["There is a place in the city centre, Alimentum, that is not family-friendly."]
-            },
-            {
-                "target": ["There is a house in New Orleans."]
-            }
-        ]
-
-        self.predictions = [
-            "Alimentum is not family-friendly, and is near the Burger King in the city centre.",
-            "There is a place in the city centre, Alimentum, that is not family-friendly.",
-            "There is a house in New Orleans."
-        ]
 
         self.bleu_metric = BLEU()
 
     def test_bleu_metric_identical_pred_ref(self):
         """Tests for identical predictions and references (BLEU = 100)
         """
-        references = References({"values": self.references_identical, "language": "en"})
-        predictions = Predictions({"values": self.predictions, "language": "en"})
+
         calculated_metrics = self.bleu_metric.compute(
-            predictions=predictions, references=references)
+            references=TestData.references, predictions=TestData.identical_predictions)
         self.assertAlmostEqual(calculated_metrics["bleu"], 100.0)
 
     def test_bleu_metric_mismatched_pred_ref(self):
         """Tests for completely dissimilar predictions and references (BLEU = 0.)
         """
-        refs = [
-            {
-                "target": ["ertnec ytic eht ni gniK regruB eht raen si dna yldneirf ylimaf ton si mutnemilA"]
-            },
-            {
-                "target": ["yldneirf ylimaf ton si taht mutnemilA ertnec ytic eht ni ecalp si erehT"]
-            },
-            {
-                "target": ["snaelrO weN ni esuoh si erehT"]
-            }
-        ]
-        predictions = Predictions({"values": self.predictions, "language": "en"})
-        references = References({"values": refs, "language": "en"})
-        calculated_metrics = self.bleu_metric.compute(
-            predictions=predictions, references=references)
+        calculated_metrics = self.bleu_metric.compute(references=TestData.references,
+                                                      predictions=TestData.reversed_predictions)
         #  TODO: check why BLEU is not exactly 0 (smoothing?)
         self.assertAlmostEqual(calculated_metrics["bleu"], 0.53, places=1)
-
 
     def test_bleu_metric_mismatched_empty_tgt(self):
         """Tests for empty target
@@ -68,11 +37,10 @@ class NGramTest(unittest.TestCase):
             "",
             ""
         ]
-        predictions = Predictions({"values": tgt_empty, "language": "en"})
-        references = References({"values": self.references_identical, "language": "en"})
         calculated_metrics = self.bleu_metric.compute(
-            predictions=predictions, references=references)
+            references=TestData.references, predictions=TestData.empty_predictions)
         self.assertAlmostEqual(calculated_metrics["bleu"], 0)
+
 
 if __name__ == '__main__':
     unittest.main()
