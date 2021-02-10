0b3f4c71c5f76ef6901a5c47c6dbdbb80d1a8812
Aman Madaan <amadaan@cs.cmu.edu>
2021-02-10 12:16:35 -0500

    Create a single class for referenced tests

--- tests/test_bleu.py
+++ tests/test_bleu.py
@@ -1,66 +1,20 @@
 import unittest
-
-from numpy.lib.function_base import place
-from tests.inputs import TestData
+import gem_metrics
 from tests.test_referenced import ReferencedMetricTest
-from gem_metrics.bleu import BLEU
 
 
 
-class BleuTest2(ReferencedMetricTest):
+
+class BleuTest(ReferencedMetricTest, unittest.TestCase):
 
     def setUp(self):
         super().setUp()
+        self.metric = gem_metrics.bleu.BLEU()
         self.expected_result_basic = {'bleu': 32.56}
         self.expected_result_identical_pred_ref = {'bleu': 100}
+        #  TODO: check why BLEU is not exactly 0 (smoothing?)
         self.expected_result_mismatched_pred_ref = {'bleu': 0.505}
         self.expected_result_empty_pred_ref = {'bleu': 0}
-        
-
-class BleuTest():
-
-    test_rust_tokenizer = True
-
-    def setUp(self):
-        super().setUp()
-
-        self.bleu_metric = BLEU()
-
-    def test_bleu_metric(self):
-        """Tests for identical predictions and references (BLEU = 100)
-        """
-
-        calculated_metrics = self.bleu_metric.compute(
-            references=TestData.references, predictions=TestData.predictions)
-        self.assertAlmostEqual(calculated_metrics["bleu"], 32.56, places=2)
-
-    def test_bleu_metric_identical_pred_ref(self):
-        """Tests for identical predictions and references (BLEU = 100)
-        """
-
-        calculated_metrics = self.bleu_metric.compute(
-            references=TestData.references, predictions=TestData.identical_predictions)
-        self.assertAlmostEqual(calculated_metrics["bleu"], 100.0)
-
-    def test_bleu_metric_mismatched_pred_ref(self):
-        """Tests for completely dissimilar predictions and references (BLEU = 0.)
-        """
-        calculated_metrics = self.bleu_metric.compute(references=TestData.references,
-                                                      predictions=TestData.reversed_predictions)
-        #  TODO: check why BLEU is not exactly 0 (smoothing?)
-        self.assertAlmostEqual(calculated_metrics["bleu"], 0.505, places=2)
-
-    def test_bleu_metric_mismatched_empty_tgt(self):
-        """Tests for empty target
-        """
-        tgt_empty = [
-            "",
-            "",
-            ""
-        ]
-        calculated_metrics = self.bleu_metric.compute(
-            references=TestData.references, predictions=TestData.empty_predictions)
-        self.assertAlmostEqual(calculated_metrics["bleu"], 0)
 
 
 if __name__ == '__main__':
--- tests/test_referenced.py
+++ tests/test_referenced.py
@@ -2,54 +2,39 @@
 """
 import unittest
 from tests.inputs import TestData
-from gem_metrics.bleu import BLEU
+from tests.utils import assertDeepAlmostEqual
 
 
-class ReferencedMetricTest(unittest.TestCase):
-
-    test_rust_tokenizer = True
-
-    def setUp(self):
-        super().setUp()
-
-        self.metric = BLEU()
-        
+class ReferencedMetricTest(object):
 
     def test_metric(self):
-        """Tests for identical predictions and references (BLEU = 100)
+        """Tests for identical predictions and references
         """
 
         calculated_metrics = self.metric.compute(
             references=TestData.references, predictions=TestData.predictions)
-        self.assertDictEqual(calculated_metrics, self.expected_result_basic, places=2)
+        assertDeepAlmostEqual(self, calculated_metrics, self.expected_result_basic, places=2)
 
     def test_metric_identical_pred_ref(self):
-        """Tests for identical predictions and references (BLEU = 100)
+        """Tests for identical predictions and references
         """
-
         calculated_metrics = self.metric.compute(
             references=TestData.references, predictions=TestData.identical_predictions)
-        self.assertDictEqual(calculated_metrics, self.expected_result_identical_pred_ref)
+        assertDeepAlmostEqual(self, calculated_metrics, self.expected_result_identical_pred_ref)
 
     def test_metric_mismatched_pred_ref(self):
-        """Tests for completely dissimilar predictions and references (BLEU = 0.)
+        """Tests for completely dissimilar predictions and references
         """
         calculated_metrics = self.metric.compute(references=TestData.references,
                                                       predictions=TestData.reversed_predictions)
-        #  TODO: check why BLEU is not exactly 0 (smoothing?)
-        self.assertDictEqual(calculated_metrics, self.expected_result_mismatched_pred_ref, places=2)
+        assertDeepAlmostEqual(self, calculated_metrics, self.expected_result_mismatched_pred_ref, places=2)
 
     def test_metric_mismatched_empty_tgt(self):
         """Tests for empty target
         """
-        tgt_empty = [
-            "",
-            "",
-            ""
-        ]
         calculated_metrics = self.metric.compute(
             references=TestData.references, predictions=TestData.empty_predictions)
-        self.assertDictEqual(calculated_metrics, self.expected_result_empty_pred_ref)
+        assertDeepAlmostEqual(self, calculated_metrics, self.expected_result_empty_pred_ref)
 
 
 if __name__ == '__main__':
