152a6b2eac5e826e47be22b165748fff09bd7a38
padipadou <paul.alexis.dray@gmail.com>
2021-02-12 12:21:07 +0100

    feat: SAFEval

--- gem_metrics/__init__.py
+++ gem_metrics/__init__.py
@@ -22,6 +22,7 @@ from .msttr import MSTTR
 from .ngrams import NGramStats
 from .data import ensure_download
 from .sari import SARI
+from .safeval import SAFEval
 
 
 def metric_list_to_metric_dict(metric_list: List[str]) -> Dict[str, List]:
@@ -43,6 +44,7 @@ def metric_list_to_metric_dict(metric_list: List[str]) -> Dict[str, List]:
         'msttr': MSTTR,
         'ngram': NGramStats,
         'sari': SARI,
+        'safeval': SAFEval,
     }
 
     metric_name_to_metric_type = {
@@ -56,9 +58,10 @@ def metric_list_to_metric_dict(metric_list: List[str]) -> Dict[str, List]:
         'msttr': 'referenceless',
         'ngram': 'referenceless',
         'sari': 'sourced_and_referenced',
+        'safeval': 'sourced',
     }
 
-    referenced_list, referenceless_list, sourced_and_referenced_list = [], [], []
+    referenced_list, referenceless_list, sourced_and_referenced_list, sourced_list = [], [], [], []
 
     for metric_name in metric_list:
         metric_class = metric_name_to_metric_class[metric_name]
@@ -69,6 +72,8 @@ def metric_list_to_metric_dict(metric_list: List[str]) -> Dict[str, List]:
             referenceless_list.append(metric_class)
         elif metric_type == 'sourced_and_referenced':
             sourced_and_referenced_list.append(metric_class)
+        elif metric_type == 'sourced':
+            sourced_list.append(metric_class)
         else:
             raise NotImplementedError(f'{metric_type} is not one of [referenced, referenceless, sourced_and_referenced]. Please check the metric_name_to_metric_type dict.')
 
@@ -76,6 +81,7 @@ def metric_list_to_metric_dict(metric_list: List[str]) -> Dict[str, List]:
         'referenced_metrics': referenced_list,
         'referenceless_metrics': referenceless_list,
         'sourced_and_referenced_metrics': sourced_and_referenced_list,
+        'sourced_metrics': sourced_list,
     }
 
     return metric_dict
@@ -117,6 +123,16 @@ def compute(outs: Predictions, refs: Optional[References] = None, srcs: Optional
             logger.info(f'Computing {metric_class.__name__}...')
             metric = metric_class()
             values.update(metric.compute(outs, refs, srcs))
+
+    # compute src-based metrics
+    if srcs is not None:
+        if len(srcs) != len(outs):
+            raise ValueError(f'Incorrect length for data "{outs.filename}" -- outputs: {len(outs)} vs. sources: {len(srcs)}')
+        values['sources_file'] = srcs.filename
+        for metric_class in metrics_dict['sourced_metrics']:
+            metric = metric_class()
+            values.update(metric.compute(outs, srcs))
+
     return values
 
 
