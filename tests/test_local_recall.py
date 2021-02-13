e16b7ec27655a9891639d557de20cd1c4960e44f
Aman Madaan <amadaan@cs.cmu.edu>
2021-02-13 11:20:57 -0500

    Add tests for local recall, update ngram

---
+++ tests/test_local_recall.py
@@ -0,0 +1,16 @@
+import unittest
+import gem_metrics
+from tests.test_referenced import ReferencedMetricTest
+
+class LocalRecallTest(ReferencedMetricTest, unittest.TestCase):
+
+    def setUp(self):
+        super().setUp()
+        self.metric = gem_metrics.local_recall.LocalRecall()
+        self.expected_result_basic = {'local_recall': {1: 0.6451612903225806}}
+        self.expected_result_identical_pred_ref = {'local_recall': {1: 1.0}}
+        self.expected_result_mismatched_pred_ref = {'local_recall': {1: 0.0}}
+        self.expected_result_empty_pred_ref = {'local_recall': {1: 0.0}}
+
+if __name__ == '__main__':
+    unittest.main()
