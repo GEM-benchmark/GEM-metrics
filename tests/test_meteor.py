e57f75d3b51aa199e7e5a2dd667477be5ac5152d
Aman Madaan <amadaan@cs.cmu.edu>
2021-02-13 10:11:12 -0500

    Add unit test for meteor

---
+++ tests/test_meteor.py
@@ -0,0 +1,16 @@
+import unittest
+import gem_metrics
+from tests.test_referenced import ReferencedMetricTest
+
+class MeteorTest(ReferencedMetricTest, unittest.TestCase):
+
+    def setUp(self):
+        super().setUp()
+        self.metric = gem_metrics.Meteor()
+        self.expected_result_basic = {'meteor': 0.42}
+        self.expected_result_identical_pred_ref = {'meteor': 1.}
+        self.expected_result_mismatched_pred_ref = {'meteor': 0.}
+        self.expected_result_empty_pred_ref = {'meteor': 0.}
+
+if __name__ == '__main__':
+    unittest.main()
