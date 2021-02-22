13f2a29c3018f203b85c028e9b5dc9df23fe07c0
padipadou <paul.alexis.dray@gmail.com>
2021-02-22 10:27:20 +0100

    feat: computecached method

--- gem_metrics/safeval.py
+++ gem_metrics/safeval.py
@@ -1,43 +1,53 @@
 #!/usr/bin/env python3
 
 from .metric import SourcedMetric
-import numpy as np
-from safeval.safeval_metric import QG_policy_scorer
+from safeval.safeval_metric import QGPolicyScorer
 
 
 class SAFEval(SourcedMetric):
     def __init__(self):
-        # TODO: need to define default values
-        self.metric = QG_policy_scorer(
+        # TODO: need to define default values depending on each task (at the end)
+        self.metric = QGPolicyScorer(
             limit_sent=5,
             answer_types=['NER', 'NOUN'],
-            qg_beam_size=20,
+            need_policy=True,
+            qg_beam_size=1,
             QG_top_p=1,
             lambda_penalty=-0.05,
-            isCuda=True
-            # lang="en",
-            # task="summarization",
+            isCuda=True,
+            language="en",
+            task="summarization",
         )
-        self.task = "summarization"
         self.language = "en"
+        self.task = "summarization"
 
     def compute(self, predictions, sources):
-        if predictions.task != self.task and predictions.language != self.language:
-            # TODO: change metric in case of task change
-            # self.metric = self.metric
-            print("We need to change QA/QG models")
+        if predictions.task != self.task or predictions.language.alpha_2 != self.language:
+            self.metric = QGPolicyScorer(
+                limit_sent=5,
+                answer_types=['NER', 'NOUN'],
+                need_policy=True,
+                qg_beam_size=1,
+                QG_top_p=1,
+                lambda_penalty=-0.05,
+                isCuda=True,
+                language=predictions.language,
+                task=predictions.task,
+            )
+            self.language = predictions.language
+            self.task = predictions.task
 
-        # TODO: For better code comprehension, not batched for now.
-        scores = [
-            self.metric.compute_all(p, s)
-            for p, s in zip(predictions.untokenized, sources.untokenized)
-        ]
+        # TODO: For better code comprehension, not batched for now. (maybe in future)
+        # TODO: Add other sub metrics (precision, recall...). (at the end)
+        score = self.metric.compute_all_cached(
+            predictions.untokenized, sources.untokenized,
+            source_filename=sources.filename
+        )
 
-        # TODO: Add other sub metrics.
         return {
             'safeval': {
                 'precision': 0,
                 'recall': 0,
-                'mean': np.mean(scores)
+                'mean': score
             }
         }
