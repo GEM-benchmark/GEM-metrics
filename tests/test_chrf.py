a22e5053400f6c5598132b800ca814c3db1617ca
Nico Daheim <nico.daheim@rwth-aachen.de>
2021-10-19 16:45:12 +0200

    adds testcase

---
+++ tests/test_chrf.py
@@ -0,0 +1,33 @@
+import unittest
+import gem_metrics.chrf
+from tests.test_referenced import TestReferencedMetric
+
+
+class TestCHRF(TestReferencedMetric, unittest.TestCase):
+    def setUp(self):
+        super().setUp()
+        self.metric = gem_metrics.chrf.CHRF()
+        self.true_results_basic = {
+            "chrf": 63.61553,
+            "chrf+": 63.42195,
+            "chrf++": 60.06576
+        }
+        self.true_results_identical_pred_ref = {
+            "chrf": 100.0,
+            "chrf+": 100.0,
+            "chrf++": 100.0
+        }
+        self.true_results_mismatched_pred_ref = {
+            "chrf": 19.31088,
+            "chrf+": 16.55218,
+            "chrf++": 14.48316
+        }
+        self.true_results_empty_pred = {
+            "chrf": 0.0,
+            "chrf+": 0.0,
+            "chrf++": 0.0
+        }
+
+
+if __name__ == "__main__":
+    unittest.main()
