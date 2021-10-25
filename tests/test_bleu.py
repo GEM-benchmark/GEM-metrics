a08c1cc277a74cbfcc9fd5a3856637c88f02e5db
Nico Daheim <nico.daheim@rwth-aachen.de>
2021-10-25 19:31:24 +0200

    tests are exactly zero in new sacrebleu version

--- tests/test_bleu.py
+++ tests/test_bleu.py
@@ -8,10 +8,9 @@ class TestBleu(TestReferencedMetric, unittest.TestCase):
         super().setUp()
         self.metric = gem_metrics.bleu.BLEU()
         self.true_results_basic = {"bleu": 32.56}
-        self.true_results_identical_pred_ref = {"bleu": 100}
-        #  TODO: check why BLEU is not exactly 0 (smoothing?)
-        self.true_results_mismatched_pred_ref = {"bleu": 0.505}
-        self.true_results_empty_pred = {"bleu": 0}
+        self.true_results_identical_pred_ref = {"bleu": 100.0}
+        self.true_results_mismatched_pred_ref = {"bleu": 0.0}
+        self.true_results_empty_pred = {"bleu": 0.0}
 
 
 if __name__ == "__main__":
