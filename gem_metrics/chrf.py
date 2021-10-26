4d9dc4bcdaecc29827e1d7bbd57aadf12aa67400
Nico Daheim <nico.daheim@rwth-aachen.de>
2021-10-26 20:17:13 +0200

    reviewer comments

--- gem_metrics/chrf.py
+++ gem_metrics/chrf.py
@@ -7,7 +7,34 @@ from itertools import zip_longest
 import sacrebleu
 
 class CHRF(ReferencedMetric):
-    """CHRF from SacreBLEU."""
+    """
+    Computes CHRF, CHRF+ and CHRF++.
+
+    CHRF is a language-agnostic metric introduced in:
+   
+    CHRF: character n-gram F-score for automatic MT evaluation
+    Maja Popovic 
+    Proceedings of the Tenth Workshop on Statistical Machine Translation, ACL, 2015
+    https://aclanthology.org/W15-3049.pdf
+   
+    CHRF calculates an F-score based on character n-gram precision (CHRP) and 
+    character n-gram recall (CHRR) as:
+    CHRF = (1+beta^2) * ((CHRP * CHRR) / (beta^2 * (CHRP + CHRR))
+    This implementation wraps sacrebleu (https://github.com/mjpost/sacrebleu) and uses
+    beta = 2 as well as epsilon smoothing similar to the reference implementation.
+    Character n-grams up to the order of 6 are considered.
+
+
+    CHRF++ was introduced in:
+
+    CHRF++: words helping character n-grams
+    Maja Popovic
+    Proceedings of the Conference on Machine Translation (WMT), ACL, 2017
+    https://aclanthology.org/W17-4770.pdf
+
+    and adds word unigrams and bigrams to the metric.
+    In CHRF+, only unigrams are added.
+    """
 
     def support_caching(self):
         # corpus-level, so individual examples won't be aggregated.
@@ -25,6 +52,6 @@ class CHRF(ReferencedMetric):
                 word_order=word_order,
                 eps_smoothing=True
             )
-            scores[key] = round(chrf.score, 5)
+            scores[key] = chrf.score
 
         return scores
