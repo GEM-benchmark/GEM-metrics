cd7d0a31a1e43fd00df0a37786f1dd351eed8f55
Daniel Deutsch <danfdeutsch@gmail.com>
2022-02-07 12:37:31 -0500

    Adding testing device parameter

--- tests/test_bleurt.py
+++ tests/test_bleurt.py
@@ -2,6 +2,7 @@ import unittest
 import sys
 import gem_metrics.bleurt
 from tests.test_referenced import TestReferencedMetric
+from tests.utils import get_testing_device
 
 sys.argv = sys.argv[:1]  # ignore unittest flags
 
@@ -9,7 +10,8 @@ sys.argv = sys.argv[:1]  # ignore unittest flags
 class TestBluert(TestReferencedMetric, unittest.TestCase):
     def setUp(self):
         super().setUp()
-        self.metric = gem_metrics.bleurt.BLEURT()
+        device = get_testing_device()
+        self.metric = gem_metrics.bleurt.BLEURT(device=device)
         self.metric._initialize()
         self.true_results_basic = {"bleurt": -0.018821040789286297}
         self.true_results_identical_pred_ref = {"bleurt": 0.941490113735199}
